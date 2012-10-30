b8526f3cf2af5521963eac2ded57385e03d699c9
naota70 <seventy7031@gmail.com>
2012-10-30 12:24:47 +0900

    test

--- app.js
+++ app.js
@@ -3,16 +3,18 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var port = 8124
+	, express = require('express')
+	, routes = require('./routes')
+	, user = require('./routes/user')
+	, http = require('http')
+	, path = require('path')
+	, io = require('socket.io').listen(port);
 
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || port);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
   app.use(express.favicon());
@@ -28,8 +30,26 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
+app.get('/admin', function(req, res){
+	res.render('admin', {});
+});
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(3000, function(){
   console.log("Express server listening on port " + app.get('port'));
+});
+
+// socket.io
+io.sockets.on('connection', function(socket) {
+	console.log('connect');
+	
+	// 自分を以外の全クライアントにブロードキャストする
+	socket.on('updateCode', function(data){
+		// 自分を含む全クライアントにブロードキャストする
+		socket.broadcast.emit('sendingCode', data);
+	});
+	
+	socket.on('disconnect', function(){
+		console.log('disconnect');
+	});
 });
---
+++ public/javascripts/client.js
@@ -0,0 +1,4 @@
+var socket = io.connect('http://49.212.196.112:8124')
+  , timer
+  , TIPING = 'typing'
+  , POST = 'post';
